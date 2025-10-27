a4f8f6850a51391ad09b2022126a8ec5861ffd52
Амантур Эркинов <amanturerkinov@Noutbuk-Amantur.local>
2025-10-27 18:11:36 +0600

    cc

--- kunguroff/clients/models.py
+++ kunguroff/clients/models.py
@@ -24,15 +24,15 @@ class Trustor(models.Model):
     email = models.EmailField(blank=True, verbose_name="Email")
     
     # Паспортные данные
-    passport_series = models.CharField(max_length=4, verbose_name="Серия паспорта")
-    passport_number = models.CharField(max_length=6, verbose_name="Номер паспорта")
+    passport_series = models.CharField(max_length=14, verbose_name="Серия паспорта")
+    passport_number = models.CharField(max_length=7, verbose_name="Номер паспорта")
     passport_issued_by = models.TextField(verbose_name="Кем выдан")
     passport_issue_date = models.DateField(verbose_name="Дата выдачи")
     registration_address = models.TextField(verbose_name="Адрес регистрации")
     residence_address = models.TextField(blank=True, verbose_name="Адрес проживания")
     
     # Дополнительная информация
-    inn = models.CharField(max_length=12, blank=True, verbose_name="ИНН")
+    inn = models.CharField(max_length=14, blank=True, verbose_name="ИНН")
     notes = models.TextField(blank=True, verbose_name="Заметки")
     
     # Связи
