aba1030da68095bde9a67254327e52156c0736ed
Амантур Эркинов <amanturerkinov@Noutbuk-Amantur.local>
2026-04-17 22:48:32 +0600

    sdfsdfw32r34r23r

--- kunguroff/kunguroff/settings.py
+++ kunguroff/kunguroff/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.humanize',
     'django.contrib.staticfiles',
-    'cases',
+    'cases.apps.CasesConfig',
     'clients',
     'widget_tweaks',
     'core',
